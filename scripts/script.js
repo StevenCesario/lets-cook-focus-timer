1399448b9d131ee4c7800e0a4bc1484d17e1ef47
StevenCesario <steven.lennartsson@gmail.com>
2026-03-01 17:10:18 +0000

    formatTime helper method using Math.floor, modulo, and String.padStart

--- scripts/script.js
+++ scripts/script.js
@@ -12,5 +12,19 @@ const StateBuffer = {
 
 // VIEW RENDERER (Our "Mirror")
 const ViewRenderer = {
-    // To be added
+    // Helper method to format raw seconds into "MM:SS"
+    formatTime(seconds) {
+        // Math.floor chops off any decimals to give us whole minutes
+        const m = Math.floor(seconds / 60);
+
+        // And modulo gives us the remining seconds after dividing by 60
+        const s = seconds & 60;
+
+        // Convert the numbers to strings and pad them to guarantee 2 characters
+        // Same method that I used in Vibe Salad!
+        const formattedMinutes = String(m).padStart(2, '0');
+        const formattedSeconds = String(s).padStart(2, '0');
+
+        return `${formattedMinutes}:${formattedSeconds}`;
+    },
 }
